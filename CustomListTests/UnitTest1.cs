d2afc59867b63fd2b8d20f34839be85f14937c5c
JoseTorresDevCode <SgtJoseTorres@yahoo.com>
2020-08-24 11:42:58 -0500

    added unit test and updated reference

---
+++ CustomListTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomListClassProject;
+
+namespace CustomListTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
