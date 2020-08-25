c840b0b9a32afa129d57e51b938d2ff3e3591496
JoseTorresDevCode <SgtJoseTorres@yahoo.com>
2020-08-25 14:40:11 -0500

    added two remove tests

--- CustomListTests/AddTesting.cs
+++ CustomListTests/AddTesting.cs
@@ -8,23 +8,7 @@ namespace CustomListTests
     public class AddTesting
     {
         [TestMethod]
-        public void Add_AddTwoValues_CountOf2()
-        {
-            //arrange
-            CustomList<int> customList = new CustomList<int>();            
-            int value1 = 1;
-            int value2 = 2;
-            int expected = 2;
-            int actual;
-
-            //act
-            customList.Add(value1);
-            customList.Add(value2);
-            actual = customList.Count;
-
-            //assert
-            Assert.AreEqual(expected, actual);
-        }
+       
         [TestMethod]
         public void Add_AddTwo_CapacityOf4()
         {
---
+++ CustomListTests/RemoveTesting.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomListClassProject;
+
+namespace CustomListTests
+{
+    [TestClass]
+    public class RemoveTesting
+    {
+        [TestMethod]
+        public void Remove_Removeindex_TotalCountAfterRemoval()
+        {
+            //arrange
+            CustomList<int> customList = new CustomList<int>();
+            int value1 = 1;
+            int value2 = 2;
+            int expected = 1;
+            int actual;
+
+            //act
+            customList.Add(value1);
+            customList.Add(value2);
+            customList.RemoveAt(value2);
+            actual = customList.Count;
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Remove_AtIndex0_NewValueAtIndex0()
+        {
+            //arrange
+            CustomList<int> customList = new CustomList<int>();
+            int value1 = 1;
+            int value2 = 2;
+            int expected = 2;
+            int actual;
+
+            //act
+            customList.Add(value1);
+            customList.Add(value2);
+            customList.RemoveAt(0);
+            actual = customList[0];
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
