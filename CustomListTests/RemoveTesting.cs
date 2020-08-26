58ca01cbfefd22290876013c40946f1c039e0801
JoseTorresDevCode <SgtJoseTorres@yahoo.com>
2020-08-26 11:37:18 -0500

    added new remove test

--- CustomListTests/RemoveTesting.cs
+++ CustomListTests/RemoveTesting.cs
@@ -20,7 +20,7 @@ namespace CustomListTests
             //act
             customList.Add(value1);
             customList.Add(value2);
-            customList.RemoveAt(value2);
+            customList.Remove(value2);
             actual = customList.Count;
 
             //assert
@@ -40,8 +40,52 @@ namespace CustomListTests
             //act
             customList.Add(value1);
             customList.Add(value2);
-            customList.RemoveAt(0);
+            customList.Remove(0);
             actual = customList[0];
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Remove_AtIndex1_NewValueOutofRange()
+        {
+            //arrange
+            CustomList<int> customList = new CustomList<int>();
+            int value1 = 1;
+            int value2 = 2;
+            int actual;
+
+            //act
+            customList.Add(value1);
+            customList.Add(value2);
+            customList.Remove(1);
+            actual = customList[1];
+
+        }
+
+        [TestMethod]
+        public void Remove_ReduceCapacity_CapacityReturnsto4()
+        {
+            //arrange
+            CustomList<int> customList = new CustomList<int>();
+            int value1 = 1;
+            int value2 = 2;
+            int value3 = 3;
+            int value4 = 4;
+            int value5 = 5;
+            int expected = 4;
+            int actual;
+
+            //act
+            customList.Add(value1);
+            customList.Add(value2);
+            customList.Add(value3);
+            customList.Add(value4);
+            customList.Add(value5);
+            customList.Remove(value1);
+            actual = customList.Capacity;
 
             //assert
             Assert.AreEqual(expected, actual);
